4d90e7c483ed531f4257ecc1a061feb25859f365
sam <Samdhanaseelan2636@gmail.com>
2022-06-16 17:04:22 +0530

    routing done

--- react-parallex/src/App.js
+++ react-parallex/src/App.js
@@ -1,34 +1,15 @@
 
 import './App.css';
-import {Parallax} from 'react-parallax';
-import moon from './images/moon.jpg';
-import star from './images/star.jpg';
-import uni from './images/uni.jpg';
+import Parallaxfile from './parallaxflie';
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Parallax strength={600} bgImage={moon}>
-        <div className="content">
-          <div className="text-content">Normal parallax</div>
-        </div>
-      </Parallax> 
-
-      <Parallax strength={300} blur={{min:-15,max:15}} bgImage={star}>
-        <div className="content">
-          <div className="text-content">blur parallax</div>
-        </div>
-      </Parallax> 
-
-      <Parallax strength={-600} bgImage={uni}>
-        <div className="content">
-          <div className="text-content">reverse parallax</div>
-        </div>
-      </Parallax> 
-
-      <div className="content">
- 
-      </div>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/app" element={<Parallaxfile/>} />
+      </Routes>    
+    </Router>
   );
 }
 
