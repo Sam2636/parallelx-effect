c29c8e35608ac794213b9fa9d808cb624b28a89f
sam <Samdhanaseelan2636@gmail.com>
2022-06-17 00:13:52 +0530

    redirect pages is done

--- react-parallex/src/pages/animation/animationfile.js
+++ react-parallex/src/pages/animation/animationfile.js
@@ -1,9 +1,21 @@
 import './animation.css';
-
+import React from "react"
+import { useNavigate } from 'react-router-dom';
 
 function Animation() {
+
+  const navigate = useNavigate();
   return (
-    <div>hello world</div>
+    <div>hello world
+      <div>
+        <button onClick={() => {
+          navigate('/app');
+        }}
+        >
+        go to app</button>
+      </div>
+    </div>  
+
   );
 }
 
