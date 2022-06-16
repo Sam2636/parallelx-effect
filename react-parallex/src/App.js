3671ef13e9a547d073e5bffcff7f5672d665d3ad
sam <Samdhanaseelan2636@gmail.com>
2022-06-16 13:37:00 +0530

    adding files

--- react-parallex/src/App.js
+++ react-parallex/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {Parallax} from 'react-parallax';
+import moon from './images/moon.jpg';
+import star from './images/star.jpg';
+import uni from './images/uni.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={600} bgImage={moon}>
+        <div className="content">
+          <div className="text-content">Normal parallax</div>
+        </div>
+      </Parallax> 
+
+      <Parallax strength={300} blur={{min:-15,max:15}} bgImage={star}>
+        <div className="content">
+          <div className="text-content">blur parallax</div>
+        </div>
+      </Parallax> 
+
+      <Parallax strength={-600} bgImage={uni}>
+        <div className="content">
+          <div className="text-content">reverse parallax</div>
+        </div>
+      </Parallax> 
+
+      <div className="content">
+ 
+      </div>
     </div>
   );
 }
