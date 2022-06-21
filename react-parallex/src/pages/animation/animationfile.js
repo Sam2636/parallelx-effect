b656effb8bfea6c7dc34feb4d24073303e1a5e1a
sam <Samdhanaseelan2636@gmail.com>
2022-06-21 18:50:28 +0530

    hover button effects

--- react-parallex/src/pages/animation/animationfile.js
+++ react-parallex/src/pages/animation/animationfile.js
@@ -7,22 +7,25 @@ function Animation() {
   const navigate = useNavigate();
   return (
     // <div class="zoom"><img src='uni'></img>
-    <div>hello world
-      <div>
-        <button onClick={() => {
-          navigate('/app');
-        }}
-        >
-        go to app</button>
-      </div>
-      <div>
-        <button onClick={() => {
-          navigate('/video_animation');
-        }}
-        >
-        video_animation effects</button>
-      </div>
-    </div>  //</div>
+    <>
+      <h1 class="hi"> Hello </h1>
+      <div class="hello">
+        <div>
+          <button class="glow-on-hover" onClick={() => {
+            navigate('/app');
+          }}
+          >
+          go to app</button>
+        </div>
+        <div>
+          <button class="glow-on-hover" onClick={() => {
+            navigate('/video_animation');
+          }}
+          >
+          video_animation effects</button>
+        </div>
+      </div>  
+    </>
 
   );
 }
